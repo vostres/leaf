4ce78391ae0b019379f7fe17f0b6b2131e9b1f32
Zhenia <zhenia+github@posteo.de>
2024-02-01 11:33:42 +0100

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='leafxai',
+    version='0.1.0',
+    description='A Python framework for the quantitative evaluation of eXplainable AI methods.',
+    author='@amparore',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.0',
+        'pandas>=1.0',
+        'lime>=0.2.0',
+        'shap>=0.39.0',
+        'imbalanced-learn>=0.8.0',
+        'tabulate>=0.8.9',
+    ],
+)
