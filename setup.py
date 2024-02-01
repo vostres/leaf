6e03b93b852c000fa24b885ab7e98f8d6cceb408
Zhenia <zhenia+github@posteo.de>
2024-02-01 11:42:04 +0100

    add __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='leafxai',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python framework for the quantitative evaluation of eXplainable AI methods.',
     author='@amparore',
     packages=find_packages(),
